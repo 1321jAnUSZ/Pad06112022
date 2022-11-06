c991e3f6525fd801c0d960b987817962101eb75c
1321jAnUSZ <janusz.ak.1321@gmail.com>
2022-11-06 18:51:17 +0100

    Dodaj pliki projektów.

---
+++ PADI/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PADI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightBlue;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(596, 515);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(263, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(322, 49);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Zmiana kolorów na szary";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(213, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 79);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Usuwanie";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(213, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(356, 79);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sortowanie";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(213, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Losuj";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(183, 358);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 514);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Losowanie i sortowanie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ PADI/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace PADI
+{
+    public partial class Form1 : Form
+    {
+        static int rozmiar = 1;
+        Random random = new Random();
+        int[] t1 = new int[rozmiar];
+
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i = 0; i < t1.Length; i++)
+            {
+                t1[i] = random.Next(0,2500);
+            }
+        }
+        void Wypisz(Array array, RichTextBox RTB)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                RTB.AppendText(array.GetValue(i).ToString());
+                RTB.Text += "\n";
+            }
+        }
+        
+        void zwiekszanieRozmaru(ref int[] t, int rozmiar)
+        {
+            int staryRozmiar = t.GetLength(0);
+            Array.Resize(ref t, rozmiar);
+            for(int i = staryRozmiar; i< rozmiar;i++)
+            {
+                t[i] = random.Next(0,2500);
+            }
+        }
+
+        void Sort(ref int [] t,RichTextBox RTB)
+        {
+            Array.Sort(t);
+            Wypisz(t, RTB);
+        }
+        void DeleteNum()
+        {
+            richTextBox1.Text = " ";
+        } 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rozmiar = Convert.ToInt32(textBox1.Text);
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            zwiekszanieRozmaru(ref t1, t1.Length + rozmiar);
+            Wypisz(t1, richTextBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sort(ref t1,richTextBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DeleteNum();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == Color.Lime)
+            {
+                panel1.BackColor = Color.DarkGray;
+                button1.BackColor = Color.Gray;
+                button2.BackColor = Color.Gray;
+                button3.BackColor = Color.Gray;
+                button4.BackColor = Color.Gray;
+                textBox1.BackColor = Color.Gray;
+                richTextBox1.BackColor = Color.Gray;
+            }
+                else
+            {
+                panel1.BackColor = Color.LightBlue; 
+                button1.BackColor = Color.Lime;
+                button2.BackColor = Color.DarkOrange;
+                button3.BackColor = Color.Red;
+                button4.BackColor = Color.Yellow;
+                textBox1.BackColor = Color.White;
+                richTextBox1.BackColor = Color.White;
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Wypisz(t1, richTextBox1);
+        }
+    }
+}
